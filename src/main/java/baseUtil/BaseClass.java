52fdfbbd3d91fd9a64c0947a4df8b7355de58640
Mizrman <mdmzrmn@gmail.com>
2022-10-04 11:12:26 -0400

    hw22

--- src/main/java/baseUtil/BaseClass.java
+++ src/main/java/baseUtil/BaseClass.java
@@ -10,18 +10,20 @@ import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 
 public class BaseClass {
-	WebDriver driver;
+	protected WebDriver driver;
 	protected HomePage homePage;
 	
 	@BeforeMethod
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "/Users/Miz/eclipse-workspace/com.verizon/driver/chromedriver");
-		driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "/Users/Miz/eclipse-workspace/com.verizon/driver/chromedriver");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver");
+		driver = new ChromeDriver(); 
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.get("https://www.verizon.com/");
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
 		homePage = new HomePage(driver);
 	}
 
--- src/test/java/testPages/HomePageTest.java
+++ src/test/java/testPages/HomePageTest.java
@@ -1,50 +1,91 @@
 package testPages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 import baseUtil.BaseClass;
 
 public class HomePageTest extends BaseClass{
-	WebDriver driver;
 	
-	@Test (enabled = true, priority = 1)
+	@Test (enabled = false)
 	public void logoTest() throws InterruptedException {
 		homePage.logoDisplayed();
 	}
 	
-	@Test (enabled = true, priority = 4)
+	@Test (enabled = false)
 	public void clickWhySupportUsTest() throws InterruptedException {
 		homePage.clickWhySupportUs();
 	}
 	
-	@Test (enabled = true, priority = 2)
+	@Test (enabled = false)
 	public void signInTest() throws InterruptedException {
 		homePage.signIn();
 	}
 	//
 	
-	@Test (enabled = true, priority = 3)
+	@Test (enabled = false)
 	public void clickRegisterTest() throws InterruptedException {
 		homePage.clickRegister();
 	}
-	@Test (enabled = true, priority = 5)
+	@Test (enabled = false)
 	public void forgotYourInfoClickedTest() throws InterruptedException {
 		homePage.forgotYourInfoClicked();
 	}
 	
-	@Test (enabled = true, priority = 6)
+	@Test (enabled = false)
 	public void businessSignInTest() throws InterruptedException {
 		homePage.businessSignIn();
 	}
 	
-	@Test (enabled= true, priority = 7)
+	@Test (enabled= false)
 	public void makeOneTimePaymentTest() {
 	driver.findElement(By.xpath("//a [text() ='Make a one-time payment']")).click();
 	// This way will be used a lot in order to find elements
-		
 	}
+	
+	@Test (enabled= false)
+	public void searchFieldTest() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.navigate().to("https://www.hbo.com/");
+		driver.findElement(By.id("page17387-band61927-Header61928-Button-secondary-cta-default")).click();
+		Thread.sleep(3000);
+		}
+	
+	@Test (enabled= false)
+	public void signInClickTest() throws InterruptedException {
+		driver.navigate().to("https://www.hbo.com/");
+		driver.findElement(By.id("page17387-band61927-Header61928-Button-secondary-cta-default")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector("a.btn.btn--hbomax.btn-undefined.btn-sm")).click();
+		Thread.sleep(4000);
+	}
+	
+	@Test (enabled= false)
+	public void searchMoviesTest() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.navigate().to("https://www.hbo.com/");
+		driver.findElement(By.linkText("MOVIES")).click();
+		//WebElement movies= driver.findElement(By.linkText("MOVIES"));
+		//movies.click();
+		Thread.sleep(3000);
+	}
+	@Test  (enabled= false)
+	public void cssSelectorIdTest() throws InterruptedException {
+		driver.navigate().to("https://www.hbo.com/");
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("div#page17387-band218542-Text218544")).isDisplayed();
+		Thread.sleep(5000);
+	}
+	
+	@Test (enabled= false)
+	public void cookiesSettingTest () throws InterruptedException {
+		driver.navigate().to("https://www.hbo.com/");
+		Thread.sleep(3000);
+		driver.findElement(By.partialLinkText("Cookies Set")).isEnabled();
+		Thread.sleep(3000);
+	}
+	//
+	
 	
 	
 }
