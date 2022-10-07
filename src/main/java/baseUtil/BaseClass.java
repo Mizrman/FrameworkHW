b9bcb2fed70accdeb24ebed110e13acdd2fd4223
Mizrman <mdmzrmn@gmail.com>
2022-10-07 12:47:43 -0400

    hw23

--- src/main/java/baseUtil/BaseClass.java
+++ src/main/java/baseUtil/BaseClass.java
@@ -4,21 +4,26 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import pages.HomePage;
 
 public class BaseClass {
-	protected WebDriver driver;
-	protected HomePage homePage;
+	public WebDriver driver;
+	public HomePage homePage;
 	
 	@BeforeMethod
 	public void setUp() {
 		//System.setProperty("webdriver.chrome.driver", "/Users/Miz/eclipse-workspace/com.verizon/driver/chromedriver");
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver");
-		driver = new ChromeDriver(); 
+		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver");
+		 //System.setProperty("webdriver.gecko.driver", "./driver/geckodriver 2");
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+		driver = new ChromeDriver();
+		//driver = new FirefoxDriver();
 		driver.manage().window().maximize();
+		//driver.manage().window().fullscreen(); (does not work in mac sometimes and may work in firefox)
 		driver.manage().deleteAllCookies();
 		driver.get("https://www.verizon.com/");
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
--- src/test/java/testPages/HomePageTest.java
+++ src/test/java/testPages/HomePageTest.java
@@ -1,6 +1,10 @@
 package testPages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.service.DriverCommandExecutor;
 import org.testng.annotations.Test;
 
 import baseUtil.BaseClass;
@@ -84,8 +88,80 @@ public class HomePageTest extends BaseClass{
 		driver.findElement(By.partialLinkText("Cookies Set")).isEnabled();
 		Thread.sleep(3000);
 	}
-	//
 	
+	@Test (enabled= false)
+	public void isDisplayedTest() throws InterruptedException {
+		driver.get("https://www.walgreens.com/");
+		Thread.sleep(3000);
+		driver.findElement(By.linkText("icon icon__wag-corner-flag")).isDisplayed();
+		Thread.sleep(3000);
+	}
 	
+	@Test (enabled= false)
+	public void isEnabledTest() {
+	boolean name= driver.findElement(By.xpath("//a[contains(@class,'Button Button--seconda')]")).isEnabled();
+		System.out.println("This is the isEnabled Test" + name);
+		
+	}
+	
+	@Test (enabled= false)
+	public void checkboxSelectedTest() {
+		driver.get("https://www.capitalone.com/");
+		driver.findElements(By.id("rememberMe")).isSelected();
+		//isSelected(); method not working. Please take a look!
+	}
+	
+	@Test (enabled= false)
+	public void getTitledTest() {
+		System.out.println("The title of this website is: " + driver.getTitle());
+		}
+	@Test (enabled= false)
+	public void getCurrentUrlTest() {
+		driver.get("https://www.capitalone.com/");
+		driver.findElement(By.id("forgotUnpw")).click();
+		System.out.println("The current URL is: " + driver.getCurrentUrl());
+		
+	}
+	
+	@Test (enabled= false)
+	public void getTextTest() {
+		driver.get("https://www.capitalone.com/");
+		WebElement name = driver.findElement(By.xpath("//span[text()='Learn & Grow']"));
+		System.out.println(name.getText());
+	}
+	@Test (enabled= false)
+	public void getAttributeTest() {
+		String value = driver.findElement(By.xpath("//button[@id='gnav20-Why-Verizon-L1']")).getAttribute("id");
+		String value1 = driver.findElement(By.xpath("//button[@id='gnav20-Why-Verizon-L1']")).getAttribute("aria-label");
+		String value2 = driver.findElement(By.xpath("//button[@id='gnav20-Why-Verizon-L1']")).getAttribute("class");
+		System.out.println("Value of attribute id is: " + value);
+		System.out.println("The value of aria label is: " + value1);
+		System.out.println(value2);
+	}
+	@Test (enabled= false)
+	public void sendKeysTest() throws InterruptedException {
+		driver.findElement(By.id("gnav20-search-icon")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.id("search_box_gnav_input")).sendKeys("iphone 14",Keys.RETURN);
+		Thread.sleep(3000);
+		driver.findElement(By.id("search_box_one_search_input")).clear();
+		Thread.sleep(2000);
+		driver.findElement(By.id("search_box_one_search_input")).sendKeys("Samsung",Keys.RETURN);
+}
+	@Test (enabled= false)
+	public void dimensionTest() {
+		Dimension dimesnion = new Dimension(800, 600);
+		driver.manage().window().setSize(dimesnion);
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.hbo.com/");
+		System.out.println("Size of the window is: " + driver.manage().window().getSize());
+	}
+	@Test (enabled= false)
+	public void navigateForwardBackTest() {
+		driver.navigate().to("https://www.hbo.com/");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+	}
 	
 }
